4ca746afd49e15a3b195446e19ec549cd7173d10
Artur Shkekin <64356091+ArturShkekin@users.noreply.github.com>
2021-03-25 21:50:22 +0200

    добавил 2 задачи

--- js/task-01.js
+++ js/task-01.js
@@ -2965,6 +2965,29 @@ makePizza('Роял гранд', function deliverPizza(pizzaName) {console.log(`
 makePizza('Ультрасыр', function eatPizza(pizzaName){console.log(`Едим пиццу ${pizzaName}.`);});*/
 
 
+// Задача 4 модуль 4 
+/*const pizzaPalace = {
+pizzas: ['Ультрасыр', 'Аль Копчино', 'Четыре нарезона'],
+  order(pizzaName, makePizza, onOrderError) {
+  if (!this.pizzas.includes(pizzaName)) {
+    return onOrderError(`В ассортименте нет пиццы с названием ${pizzaName}.`);
+}
+    return makePizza(pizzaName); 
+  },
+}
+// Пиши код выше этой строки
+// Колбэк для onSuccess
+function makePizza(pizzaName) {
+  return `Ваш заказ принят. Готовим пиццу ${pizzaName}.`;
+}
+// Колбэк для onError
+function onOrderError(error) {
+  return `Ошибка! ${error}`;
+}*/
+
+
+
+
 
 
 
@@ -2972,6 +2995,63 @@ makePizza('Ультрасыр', function eatPizza(pizzaName){console.log(`Еди
 /*const print = (info) => {
   console.log(info)
 }*/
+
+
+
+// Задача 7 модуль 4 
+//Сервису приготовления и доставки еды требуется функция генерации сообщений о статусе заказа.
+//Дополни функцию composeMessage(position) так, чтобы она возвращала строку в формате 'Готовим <блюдо> для <почта>. Ваш заказ <позиция>-й в очереди.'
+// Позиция это значение параметра position - позиция элемента в массиве (на единицу больше чем индекс).
+//Не объявляй дополнительные параметры функции composeMessage(position).
+//Используй call для вызова функции в контексте одного объекта-заказа.
+//Используй this в теле функции для доступа к свойствам объекта-заказа в контексте которого она была вызывана.
+//Дополни код так, чтобы в переменной messages, с помощью метода map, получился массив сообщений о статусе заказов из массива orders.
+
+
+/*const orders = [
+  { email: 'solomon@topmail.ua', dish: 'Burger' },
+  { email: 'artemis@coldmail.net', dish: 'Pizza' },
+  { email: 'jacob@mail.com', dish: 'Taco' },
+];
+
+// Пиши код ниже этой строки ВАРИАНТ 1
+function composeMessage(position) {
+  return `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.` ;
+}
+const messages = orders.map((order, i) => composeMessage.call(order, i + 1));
+
+console.log(messages);*/
+
+
+
+// Пиши код ниже этой строки ВАРИАНТ 2
+/*function composeMessage(position) {
+  return `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`;
+}
+
+const messages = orders.map((order, index) => composeMessage.call(order, index + 1));*/
+
+
+
+
+
+// Задача 8 модуль 4 
+// Выполни рефакторинг кода так, чтобы функция composeMessage(position) вызывалась методом apply.
+/*const orders = [
+  { email: 'solomon@topmail.ua', dish: 'Burger' },
+  { email: 'artemis@coldmail.net', dish: 'Pizza' },
+  { email: 'jacob@mail.com', dish: 'Taco' },
+];
+
+// Пиши код ниже этой строки
+function composeMessage(position) {
+  return `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`;
+}
+
+const messages = orders.map((order, index) =>
+  composeMessage.apply(order, [index + 1])
+);*/
+
 
 
 
