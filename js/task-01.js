05b72499efa08d412bb1b7a2744589636f536be3
Artur Shkekin <64356091+ArturShkekin@users.noreply.github.com>
2021-04-02 21:30:52 +0300

    решил три задачи

--- js/task-01.js
+++ js/task-01.js
@@ -3854,3 +3854,318 @@ console.log(storage.getItems()); // ["Нанитоиды", "Антигравит
 
 
 
+// Задача 13 модуль 5
+//Выполни рефакторинг заменив функцию-конструктор StringBuilder на класс с методами. Сделай так, чтобы свойство value было приватным.
+/*function StringBuilder(baseValue) { //Это вариант с функцией конструктором
+  this.value = baseValue;
+}
+StringBuilder.prototype.getValue = function () {
+  return this.value;
+};
+StringBuilder.prototype.padEnd = function (str) {
+  this.value += str;
+};
+StringBuilder.prototype.padStart = function (str) {
+  this.value = str + this.value;
+};
+StringBuilder.prototype.padBoth = function (str) {
+  this.padStart(str);
+  this.padEnd(str);
+};*/
+
+/*class StringBuilder { // Это вариант с классом
+#value;
+constructor(baseValue) {
+  this.#value = baseValue;
+}
+getValue(){
+  return this.#value;
+}
+padEnd(str){
+  this.#value += str;
+}
+padStart(str){
+  this.#value = str + this.#value;
+}
+padBoth(str){
+  this.padStart(str);
+  this.padEnd(str);
+}
+}
+// Пиши код выше этой строки
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // '.'
+builder.padStart('^');
+console.log(builder.getValue()); // '^.'
+builder.padEnd('^');
+console.log(builder.getValue()); // '^.^'
+builder.padBoth('=');
+console.log(builder.getValue()); // '=^.^='*/
+
+
+
+
+
+// Геттеры и сеттеры
+// Геттеры и сеттеры - это более краткий синтаксис объявления методов для взаимодействия со свойствами. Геттер и сеттер имитируют обычное публичное 
+// свойство класса, но позволяют изменять другие свойства более удобным способом. Геттер выполняется при попытке получить значение свойства, 
+//а сеттер - при попытке его изменить.
+
+// Геттеры и сеттеры хорошо использовать для простых операций чтения и изменения значения свойств, особенно приватных, как их публичный интерфейс.
+// Для работы со свойством которое хранит массив или объект они не подойдут.
+
+// Образец
+/*class User {
+  name;
+  #email;
+
+  constructor({ name, email }) {
+    this.name = name;
+    this.#email = email;
+  }
+
+  // Геттер email - для получения значения свойства,
+  get email() {
+    return this.#email;
+  }
+
+  // Сеттер email - для его изменения. // вариант 1
+  //set email(newEmail) {
+    //this.#email = newEmail;
+  //}
+  set email(newEmail) { // вариант 2
+    if(newEmail === '') {
+      console.log('Ошибка! Почта не может быть пустой строкой!');
+      return;
+    }
+  
+    this.#email = newEmail;
+  }
+
+
+}
+// Мы объявили геттер и сеттер email поставив перед именем свойства ключевые слова get и set. Внутри этих методов мы или возвращаем значение 
+// приватного свойства #email или изменяем его значение. Геттер и сеттер идут в паре и должны называться одинаково.
+
+const mango = new User({ name: 'Манго', email: 'mango@mail.com' });
+console.log(mango.email); // mango@mail.com
+mango.email = 'mango@supermail.com';
+console.log(mango.email); // mango@supermail.com*/
+
+// При обращении к mango.email вызывается геттер get email() {...} и выполняется его код. При попытке записи mango.email = 'mango@supermail.com' 
+// вызывается сеттер set email(newEmail) {...} и строка 'mango@supermail.com' будет значением параметра newEmail.
+// Плюс в том, что это методы, а значит при записи можно выполнить дополнительный код, например с какими-то проверками, 
+// в отличии от выполнениях этой же операции напрямую со свойством.
+
+
+
+
+
+// Задача 14 модуль 5
+
+// Выполни рефакторинг класса Car. Сделай свойства model и price приватными, также как #brand. Стандартизируй публичный интерфейс класса заменив 
+// уже объявленные методы на геттеры и сеттеры brand, model и price для взаимодействия с приватными свойствами.
+
+
+/*class Car {
+  #model;
+  #price;
+  #brand;
+
+  constructor({ brand, model, price }) {
+    this.#brand = brand;
+    this.#model = model;
+    this.#price = price;
+  }
+
+  get brand() {  // пишем с маленькой буквы
+    return this.#brand;
+  }
+
+  set brand(newBrand) {
+    this.#brand = newBrand;
+  }
+
+  get model() {
+    return this.#model;
+  }
+
+  set Model(newModel) {
+    this.#model = newModel;
+  }
+
+  get price() {
+    return this.#price;
+  }
+
+  set price(newPrice) {
+    this.#price = newPrice;
+  }
+}*/
+
+
+
+
+
+// Статические свойства
+// Кроме публичных и приватных свойств будущего экземпляра, в классе можно объявить его собственные свойства, доступные только классу, 
+// но не его экземплярам - статические свойства (static). Они полезны для хранения информации относящейся к самому классу.
+// Добавим классу пользователя приватное свойство type - его тип, определяющий набор прав, например администратор, редактор, просто пользователь 
+// и т п. Возможные типы пользователей будем хранить как статическое свойство TYPES - объект со свойствами.
+// Статические свойства объявляются в теле класса. Перед именем свойства добавляется ключевое слово static.
+
+
+// Образец
+/*class User {
+  // Объявление и инициализация статического свойства
+  static TYPES = {
+    ADMIN: 'admin',
+    EDITOR: 'editor',
+  };
+  #email;
+  #type;
+
+  constructor({ email, type }) {
+    this.#email = email;
+    this.#type = type;
+  }
+
+  get type() {
+    return this.#type;
+  }
+
+  set type(newType) {
+    if (User.TYPES[newType] === undefined) {
+      console.log('Ошибка! Такого типа пользователя не существет');
+      return;
+    }
+
+    this.#type = newType;
+  }
+}
+
+const mango = new User({
+  email: 'mango@mail.com',
+  type: User.TYPES.ADMIN,
+});
+
+console.log(mango.TYPES); // undefined
+console.log(User.TYPES); // { ADMIN: 'admin', EDITOR: 'editor' }
+
+console.log(mango.type); // admin
+mango.type = User.TYPES.EDITOR;
+console.log(mango.type); // editor*/
+
+
+
+
+
+
+
+
+// Задача 15 модуль 5
+
+// Выполни рефакторинг класса Car. Добавь публичное статическое свойство MAX_PRICE со значением 50000 - максимально допустимая цена автомобиля.
+// Добавь сеттеру price проверку передаваемого значения параметра newPrice. Если оно больше чем MAX_PRICE, сеттер ничего не делает, а если меньше
+//  или равно, то перезаписывает цену автомобиля.
+
+
+/*class Car {
+  // Пиши код ниже этой строки
+  static MAX_PRICE = 50000; // Поменял цену
+  #price;
+
+  constructor({ price }) {
+    this.#price = price;
+  }
+
+  get price() {
+    return this.#price;
+  }
+
+  set price(newPrice) {
+    if(newPrice < Car.MAX_PRICE) { // Добавил это и все 
+      this.#price = newPrice;
+    }
+    return;
+  }
+  // Пиши код выше этой строки
+}
+
+const audi = new Car({price: 35000});
+console.log(audi.price); // 35000
+
+audi.price = 49000;
+console.log(audi.price); // 49000
+
+audi.price = 51000;
+console.log(audi.price); // 49000*/
+
+
+// Статический метод может быть приватным. В классе можно объявить не только методы будущего экземпляра, но и методы доступные только классу
+//  - статические методы, которые могут быть как публичные, так и приватные. Синтаксис объявления аналогичен статическим свойствам, за исключением
+// того, что значением будет метод.
+// Особенность статических методов в том, что во время их вызова ключевое слово this ссылается на сам класс. Это значит, что статический метод может 
+// получить доступ к статическим свойствам класса, но не к свойствам экземпляра. Логично, потому что статические методы вызывает сам класс, а не его 
+// экземпляры.
+
+/* class User {
+  static #takenEmails = []; // приватный статичесикий метод
+
+  static isEmailTaken(email) {
+    return User.#takenEmails.includes(email);
+  }
+
+  #email;
+
+  constructor({ email }) {
+    this.#email = email;
+    User.#takenEmails.push(email);
+  }
+}
+
+const mango = new User({ email: 'mango@mail.com' });
+
+console.log(User.isEmailTaken('poly@mail.com')); // false
+
+console.log(User.isEmailTaken('mango@mail.com')); // true*/
+
+
+
+
+
+
+
+// Задача 16 модуль 5
+// Добавь классу Car публичный статический метод checkPrice(price), принимающий цену автомобиля. Метод должен сравнить значения параметра price 
+// Если цена автомобиля превышает максимальную, метод должен вернуть строку 'Внимание! Цена превышает допустимую.'.
+// В противном случае метод должен вернуть строку 'Всё хорошо, цена в порядке.'.
+// Под объявлением класса мы добавили инициализацию экземпляра и вызовы методов, чтобы показать как будет использоваться метод checkPrice(price).
+
+/*class Car {
+  static #MAX_PRICE = 50000;
+  // Пиши код ниже этой строки
+static checkPrice(price) {  // Добавил это
+  if (price < Car.#MAX_PRICE) {
+   return 'Всё хорошо, цена в порядке.';
+  }
+  return 'Внимание! Цена превышает допустимую.';
+}
+  // Пиши код выше этой строки
+  constructor({ price }) {
+    this.price = price;
+  }
+}
+
+const audi = new Car({ price: 36000 });
+const bmw = new Car({ price: 64000 });
+
+console.log(Car.checkPrice(audi.price)); // Всё хорошо, цена в порядке.
+console.log(Car.checkPrice(bmw.price)); // Внимание! Цена превышает допустимую.*/
+
+
+
+
+
+// Наследование классов
+
